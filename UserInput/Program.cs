aa16dc43b11997c38dddc9089c8567e396f13be0
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-14 19:22:13 +0100

    My solution

--- UserInput/Program.cs
+++ UserInput/Program.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 
-public class TextInput { }
+public class TextInput 
+{
+    protected string value;
+    public virtual void Add(char c)
+    {
+        value += c;
+    }
 
-public class NumericInput { }
+    public string GetValue()
+    {
+        return value;
+    }
+}
+
+public class NumericInput : TextInput
+{
+    public override void Add(char c)
+    {
+        if (char.IsDigit(c))
+        {
+            value += c;
+        }
+    }
+}
 
 public class UserInput
 {
     public static void Main(string[] args)
     {
-        //TextInput input = new NumericInput();
-        //input.Add('1');
-        //input.Add('a');
-        //input.Add('0');
-        //Console.WriteLine(input.GetValue());
+        TextInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        Console.WriteLine(input.GetValue());
     }
 }
